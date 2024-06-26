a4d82666872f42c8dc246053964631120e580bff
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2024-06-26 11:27:15 +0900

    Fix test and examples

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,8 @@
-global.$ = document.querySelector.bind(document);
-global.$$ = document.querySelectorAll.bind(document);
-
 import { Application } from '@hotwired/stimulus';
 import MenuController from 'index';
 
 const application = Application.start();
 application.register('menu', MenuController);
+
+global.$ = document.querySelector.bind(document);
+global.$$ = document.querySelectorAll.bind(document);
